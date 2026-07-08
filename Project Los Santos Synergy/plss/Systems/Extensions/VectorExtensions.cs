4bc40cbd0f3bc8305e9d3f85af29e455a7d0af39
Gabrielmalaquiasarcanjo <gabrielmalaquiasaracnjo@gmail.com>
2026-07-07 23:59:54 -0300

    # Adicionar VehicleDetector com raycast capsule para detectar veículos à frente.
    # Adicionado Interção com veículo.

---
+++ Project Los Santos Synergy/plss/Systems/Extensions/VectorExtensions.cs
@@ -0,0 +1,25 @@
+﻿using GTA.Math;
+using GTA.Native;
+
+namespace Project_Los_Santos_Synergy.plss.Systems.Extensions
+{
+    internal static class VectorExtensions
+    {
+        public static bool TryWorldToScreen(this Vector3 worldPos, out float x, out float y)
+        {
+            var screenX = new OutputArgument();
+            var screenY = new OutputArgument();
+
+            bool onScreen = Function.Call<bool>(
+                Hash.GET_SCREEN_COORD_FROM_WORLD_COORD,
+                worldPos.X, worldPos.Y, worldPos.Z,
+                screenX, screenY
+            );
+
+            x = screenX.GetResult<float>();
+            y = screenY.GetResult<float>();
+
+            return onScreen;
+        }
+    }
+}
