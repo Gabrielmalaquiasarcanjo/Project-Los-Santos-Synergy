befb611b4eceafb24dc4314058b466e4c9937cf7
Gabrielmalaquiasarcanjo <gabrielmalaquiasaracnjo@gmail.com>
2026-07-08 00:12:07 -0300

    #4

--- Project Los Santos Synergy/ModController.cs
+++ Project Los Santos Synergy/ModController.cs
@@ -1,8 +1,10 @@
 ﻿using GTA;
+using GTA.Math;
 using GTA.UI;
-using Project_Los_Santos_Synergy.plss.teste.carspawn;
-using Project_Los_Santos_Synergy.plss.teste.systems;
+using Project_Los_Santos_Synergy.plss.Systems.Features.WorldSystem;
+using Project_Los_Santos_Synergy.plss.Systems.Extensions;
 using System;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Project_Los_Santos_Synergy
@@ -10,10 +12,22 @@ namespace Project_Los_Santos_Synergy
     public class ModController : Script
     {
         private DateTime _nextCheck = DateTime.MinValue;
-        private Vehicle _vehicleCache;
+        private Vehicle _vehicleOnScreen;
+        private bool _promptVisible;
 
         private readonly CarSpawn _carSpawn = new CarSpawn();
-        private readonly Systems _systems = new Systems();
+        private readonly VehicleDetector _systems = new VehicleDetector();
+
+        private readonly TextElement _promptText = new TextElement(
+            "Pressione ~INPUT_CONTEXT~ para interagir",
+            PointF.Empty,
+            0.35f,
+            Color.White,
+            GTA.UI.Font.ChaletLondon,
+            Alignment.Center,
+            true,  // shadow
+            true   // outline
+        );
 
         public ModController()
         {
@@ -26,16 +40,28 @@ namespace Project_Los_Santos_Synergy
         {
             if (DateTime.Now >= _nextCheck)
             {
-                _vehicleCache = _systems.GetVehicleInFrontOfPlayer();
+                _vehicleOnScreen = _systems.GetVehicleInFrontOfPlayer();
                 _nextCheck = DateTime.Now.AddMilliseconds(250);
             }
 
-            if (_vehicleCache != null)
+            _promptVisible = false;
+
+            if (_vehicleOnScreen != null)
             {
-                GTA.UI.Screen.ShowSubtitle($"Veículo detectado: {_vehicleCache.DisplayName}");
+                Vector3 hoodPosition = _systems.GetVehicleHoodPosition(_vehicleOnScreen);
+
+                if (hoodPosition.TryWorldToScreen(out float x, out float y))
+                {
+                    float pixelX = x * GTA.UI.Screen.Resolution.Width;
+                    float pixelY = y * GTA.UI.Screen.Resolution.Height;
+
+                    _promptText.Position = new PointF(pixelX, pixelY);
+                    _promptText.Draw();
+                    _promptVisible = true;
+                }
             }
         }
-
+    
         private void OnKeyDown(object sender, KeyEventArgs e) { }
 
         private void OnKeyUp(object sender, KeyEventArgs e)
@@ -44,6 +70,17 @@ namespace Project_Los_Santos_Synergy
             {
                 _carSpawn.CarSpawnMethod();
             }
+
+            if (e.KeyCode == Keys.E && _promptVisible && _vehicleOnScreen != null)
+            {
+                OnInteractWithVehicle(_vehicleOnScreen);
+            }
+        }
+
+        private void OnInteractWithVehicle(Vehicle vehicle)
+        {
+            GTA.UI.Notification.Show($"Interagindo com: {vehicle.DisplayName}", false);
+            // aqui entra a lógica real (entrar no carro, roubar, inspecionar, etc)
         }
     }
 }
